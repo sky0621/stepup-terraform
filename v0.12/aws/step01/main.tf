821511843b67305945f52491a81c26a7cc1e5e6a
sky0621 <sky14work@gmail.com>
2019-06-25 16:52:48 +0900

    continue

--- v0.12/aws/step01/main.tf
+++ v0.12/aws/step01/main.tf
@@ -1,11 +1,6 @@
-# https://www.terraform.io/docs/configuration/providers.html
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
 # https://www.terraform.io/docs/providers/aws/r/instance.html
 resource "aws_instance" "web-server" {
-  ami                    = "ami-0f9ae750e8274075b" # 
+  ami                    = "ami-0f9ae750e8274075b" # https://aws.amazon.com/jp/amazon-linux-2/
   instance_type          = "t2.micro"              # https://aws.amazon.com/jp/ec2/instance-types/
   vpc_security_group_ids = [aws_security_group.web-server-security-group.id]
 
